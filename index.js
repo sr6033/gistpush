b0b598e3ce6237629e9618ab1808c7b9afaa855f
Carlos Fuentes <metcdoder95@MacBook-Pro-de-Carlos.local>
2018-10-30 20:52:23 -0700

    prettier code style added

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ function main() {
       "push one or more files to github as a gist",
       { isDefault: true }
     )
-    .command("list [username]", "list all the public gist of the given user in GitHub. If no username, log in into GitHub instead to get a list of all your public/private gists on GitHub")
-		.command("delete <gist_id> [gist_id2...]", "delete one or more gists")
+    .command(
+      "list [username]",
+      "list all the public gist of the given user in GitHub. If no username, log in into GitHub instead to get a list of all your public/private gists on GitHub"
+    )
+    .command("delete <gist_id> [gist_id2...]", "delete one or more gists")
     .parse(process.argv);
 }
 
