c656d5273fe474f4ec1fbfb93a3e04e61e2eb3a1
Carlos Fuentes <metcdoder95@MacBook-Pro-de-Carlos.local>
2018-11-17 00:48:38 -0800

    Use utf-8 by default

--- utils/fs_as_promise.js
+++ utils/fs_as_promise.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 function readFile(path) {
   return new Promise((resolve, reject) => {
-    fs.readFile(path, (err, data) => {
+    fs.readFile(path, "utf-8", (err, data) => {
       if (err) reject(err);
       resolve(data);
     });
