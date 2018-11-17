e2f46cddd4107f46a9557659824df1e8fd22f56f
Carlos Fuentes <metcdoder95@MacBook-Pro-de-Carlos.local>
2018-11-17 00:48:04 -0800

    New commands added, Edit command implemented

--- index.js
+++ index.js
@@ -2,26 +2,23 @@ const program = require("commander");
 
 const { getVersion } = require("./utils");
 
-let version = "";
 (async function() {
-  version = await getVersion().catch(console.error);
+  const version = await getVersion().catch(console.error);
+
+  function main() {
+    program
+      .version(version)
+      .command(
+        "push <file1> [file2]",
+        "push one or more files to github as a gist",
+        { isDefault: true }
+      )
+      .command("list [username]", "list all gists for the given user")
+      .command("delete <gist_id...>", "delete one or more gists")
+      .command("edit <filespath...>", "update one or more files in a gist")
+      .parse(process.argv);
+  }
+
+  main();
+  module.exports = main;
 })();
-
-function main() {
-  program
-    .version(version)
-    .command(
-      "push <file1> [file2]",
-      "push one or more files to github as a gist",
-      { isDefault: true }
-    )
-    .command(
-      "list [username]",
-      "list all the public gist of the given user in GitHub. If no username, log in into GitHub instead to get a list of all your public/private gists on GitHub"
-    )
-    .command("delete <gist_id> [gist_id2...]", "delete one or more gists")
-    .parse(process.argv);
-}
-
-main();
-module.exports = main;
